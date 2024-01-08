15228adca8cf85bfad7d22be98e49a4456292ea9
strurao <strurao@gmail.com>
2024-01-09 00:23:28 +0900

    Comment

--- Assets/Scripts/FollowCameraCS.cs
+++ Assets/Scripts/FollowCameraCS.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class FollowCameraCS : MonoBehaviour
 {
     public Transform target; // 이 카메라가 따라가야 할 타겟
-    public Vector3 offset; // 오프셋
+    public Vector3 offset; // 카메라와 타겟 사이의 거리
 
     public float Yaxis;
     public float Xaxis;
 
     private float rotSensitive = 3f; // 카메라 회전 감도
-
-    private float RotationMin = -10f; // 카메라 회전각도 최소
-    private float RotationMax = 80f; // 카메라 회전각도 최대
+    private float RotationMin = -90f; // 카메라 회전각도 최소
+    private float RotationMax = 90f; // 카메라 회전각도 최대
     private float smoothTime = 0.12f; // 카메라가 회전하는데 걸리는 시간
 
     private Vector3 targetRotation;
@@ -31,7 +30,7 @@ public class FollowCameraCS : MonoBehaviour
         Yaxis = Yaxis + Input.GetAxis("Mouse X") * rotSensitive; // 마우스 좌우움직임을 입력받아서 카메라의 Y축을 회전시킨다
         Xaxis = Xaxis - Input.GetAxis("Mouse Y") * rotSensitive; // 마우스 상하움직임을 입력받아서 카메라의 X축을 회전시킨다
         
-        // X축회전이 한계치를 넘지않게 제한해준다.
+        // X축 회전 (고개 위, 아래) 이 한계치를 넘지않게 제한해준다.
         Xaxis = Mathf.Clamp(Xaxis, RotationMin, RotationMax);
 
         // SmoothDamp를 통해 부드러운 카메라 회전
