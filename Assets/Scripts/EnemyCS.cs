289432842779ba70f58209861b71c5bbf48c06e5
strurao <strurao@gmail.com>
2024-01-10 17:35:15 +0900

    Feature: Grenade Attack

--- Assets/Scripts/EnemyCS.cs
+++ Assets/Scripts/EnemyCS.cs
@@ -20,6 +20,14 @@ public class EnemyCS : MonoBehaviour
         mat = GetComponent<MeshRenderer>().material;
     }
 
+    public void HitByGrenade(Vector3 explosionPos)
+    {
+        curHealth -= 100;
+        Vector3 reactVec = transform.position - explosionPos; // 반작용 방향 구하기 (뒤로 밀려남)
+
+        StartCoroutine(OnDamage(reactVec, true));
+    }
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("EnemyCS OnTriggerEnter");
@@ -33,7 +41,7 @@ public class EnemyCS : MonoBehaviour
                 Vector3 reactVec = transform.position - other.transform.position; // 반작용 방향 구하기
 
                 Debug.Log("OnTriggerEnter Melee : " + curHealth);
-                StartCoroutine(OnDamage(reactVec));
+                StartCoroutine(OnDamage(reactVec, false));
 
             }
         }
@@ -45,16 +53,16 @@ public class EnemyCS : MonoBehaviour
             Destroy(other.gameObject); // 총알과 적이 닿는다면 총알을 제거
 
             Debug.Log("OnTriggerEnter Bullet : " + curHealth);
-            StartCoroutine(OnDamage(reactVec));
+            StartCoroutine(OnDamage(reactVec, false));
         }
     }
 
-    IEnumerator OnDamage(Vector3 reactVec)
+    IEnumerator OnDamage(Vector3 reactVec, bool isGrenade)
     {
         mat.color = Color.red;
         yield return new WaitForSeconds(0.1f);
 
-        if(curHealth > 0)
+        if (curHealth > 0)
         {
             mat.color = Color.white;
         }
@@ -64,10 +72,20 @@ public class EnemyCS : MonoBehaviour
             gameObject.layer = 12; // Layer Idx of "EnemyDead" 
 
             // 반작용 넉백
-            reactVec = reactVec.normalized;
-            reactVec += Vector3.up * 2f; // 넉백 벡터에 약간의 위 방향 추가
-            rigid.AddForce(reactVec * 5, ForceMode.Impulse);
-            
+            if (isGrenade)
+            {
+                reactVec = reactVec.normalized;
+                reactVec += Vector3.up * 3f; // 넉백 벡터에 약간의 위 방향 추가
+                
+                rigid.freezeRotation = false;
+                rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+                rigid.AddTorque(reactVec*15, ForceMode.Impulse);
+            }
+            else { 
+             reactVec = reactVec.normalized;
+             reactVec += Vector3.up * 2f; // 넉백 벡터에 약간의 위 방향 추가
+             rigid.AddForce(reactVec * 5, ForceMode.Impulse);
+            }
             Destroy(gameObject, 3);
         }
     }
