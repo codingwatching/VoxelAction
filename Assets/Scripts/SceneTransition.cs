218e31cd0a9e742087e097ed5b9aad9f5c946e06
strurao <strurao@gmail.com>
2024-01-12 13:48:14 +0900

    Test: Change Scene (+Lobby)

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -12,14 +12,32 @@ public class SceneTransition : MonoBehaviour
             string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
 
             // 씬 이름에 따라 다음 씬을 결정
-            string nextScene = currentScene == "Play01" ? "Play02" : "Play01";
+            string nextScene = currentScene;
+            Debug.Log("SceneTransition nextScene" + nextScene);
 
+            if (currentScene == "Lobby")
+            {
+                nextScene = "Play01";
+            }
+            else if (currentScene == "Play01")
+            {
+                nextScene = "Play02";
+            }
+            else if (currentScene == "Play02")
+            {
+                nextScene = "Play01";
+            }
+            // 다음 씬 로드
             UnityEngine.SceneManagement.SceneManager.LoadScene(nextScene);  // 충돌을 피하기 위해 전체 네임스페이스 사용
         }
     }
 
-/*    private void SavePlayerState(Collider player)
+    private void OnTriggerExit(Collider other)
     {
+        Debug.Log("SceneTransition OnTriggerExit");
+    }
+        /*    private void SavePlayerState(Collider player)
+            {
 
-    }*/
-}
+            }*/
+    }
